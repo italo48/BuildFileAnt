5c8bbe7d9206b21cff83afc178f8a8b88909f09e
Ítalo de Oliveira Costa <wowbrasiloficial@gmail.com>
2018-08-28 10:06:43 -0300

    Primeiro commit

---
+++ src/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("==========================+");
+		System.out.println("| Brincado com Apache Ant |");
+		System.out.println("==========================+");
+		System.out.println("|       Hello World!!     |");
+		System.out.println("==========================+");
+	}
+}
