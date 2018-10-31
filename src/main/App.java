decd60586016c0b8950b821431a8031362a87290
Ítalo De Oliveira Costa <wowbrasiloficial@gmail.com>
2018-10-31 13:32:49 -0300

    Update App.java

--- src/main/App.java
+++ src/main/App.java
@@ -3,7 +3,7 @@ package main;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("==========================+");
-		System.out.println("| Brincado com Apache Ant |");
+		System.out.println("| Brincando com Apache Ant |");
 		System.out.println("==========================+");
 		System.out.println("|       Hello World!!     |");
 		System.out.println("==========================+");
